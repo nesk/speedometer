0ca38cbff8223f98a7487225fe4f4384e5507cb3
Johann Pardanaud <pardanaud.j@gmail.com>
2015-09-29 21:57:09 +0200

    Initial Commit

---
+++ speedometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  speedometer
+//
+//  Created by Johann Pardanaud on 29/09/2015.
+//  Copyright © 2015 Johann Pardanaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
