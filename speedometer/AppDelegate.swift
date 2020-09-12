48493eb48ecff7fe3a3f9278811db75e32f5ec73
satoshi arai <jazmas74@gmail.com>
2020-09-12 12:31:13 +0900

    make it better

--- speedometer/AppDelegate.swift
+++ speedometer/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        true
+        return true
     }
 
     func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
